d11278dbe87f90ad3de6a897d993daa6479d960c
jerwin-alt <occ.buray.jerwin@gmail.com>
2026-09-21 17:59:56 +0800

    Order Imagessssssss

--- web/api/axios.jsx
+++ web/api/axios.jsx
@@ -22,7 +22,7 @@
 
 import axiosClient from "axios";
 import { getToken } from "/services/auth-storage";
-
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
 const axios = axiosClient.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
