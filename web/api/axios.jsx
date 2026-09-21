112f181b9443e98a6717aae382e34c8a9e88479c
jerwin-alt <occ.buray.jerwin@gmail.com>
2026-09-21 19:04:11 +0800

    Order Imagessssssss

--- web/api/axios.jsx
+++ web/api/axios.jsx
@@ -1,30 +1,8 @@
-// import axiosClient from "axios";
-// import { getToken } from "/services/auth-storage";
-
-// const axios = axiosClient.create({
-//   baseURL: "http://10.80.66.170:8000/api",
-//   headers: {
-//     Accept: "Content-Type: application/json",
-//   },
-// });
-
-// axios.interceptors.request.use(async (req) => {
-//   const token = await getToken();
-//   if (token) {
-//     req.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return req;
-// });
-
-// export default axios;
-
-
-
 import axiosClient from "axios";
 import { getToken } from "/services/auth-storage";
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
+
 const axios = axiosClient.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "http://10.80.66.170:8000/api",
   headers: {
     Accept: "Content-Type: application/json",
   },
@@ -39,3 +17,25 @@ axios.interceptors.request.use(async (req) => {
 });
 
 export default axios;
+
+
+
+// import axiosClient from "axios";
+// import { getToken } from "/services/auth-storage";
+// const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
+// const axios = axiosClient.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+//   headers: {
+//     Accept: "Content-Type: application/json",
+//   },
+// });
+
+// axios.interceptors.request.use(async (req) => {
+//   const token = await getToken();
+//   if (token) {
+//     req.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return req;
+// });
+
+// export default axios;
