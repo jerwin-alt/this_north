2d1cc0ed57467c04e9f36c8d9ab0d5ec8082e281
jerwin-alt <occ.buray.jerwin@gmail.com>
2026-09-23 15:52:56 +0800

    Order Imagessssssss

--- mobile/api/axios.tsx
+++ mobile/api/axios.tsx
@@ -1,8 +1,34 @@
+// import axiosClient from "axios";
+// import { getToken } from "@/services/auth-storage";
+
+// const axios = axiosClient.create({
+//   baseURL: "http://10.90.129.170:8000/api",
+//   headers: {
+//     Accept: "application/json",
+//   },
+// });
+
+// axios.interceptors.request.use(async (req) => {
+//   const token = await getToken();
+//   if (token) {
+//     req.headers.Authorization = `Bearer ${token}`;
+//     console.log("✅ Token attached to request:", req.url);
+//   } else {
+//     console.warn("⚠️ No token found for request:", req.url);
+//   }
+//   return req;
+// });
+
+// export default axios;
+
+
+
+
 import axiosClient from "axios";
 import { getToken } from "@/services/auth-storage";
 
 const axios = axiosClient.create({
-  baseURL: "http://10.90.129.170:8000/api",
+  baseURL: "https://thisnorth-production-backend.up.railway.app/api",
   headers: {
     Accept: "application/json",
   },
@@ -20,5 +46,3 @@ axios.interceptors.request.use(async (req) => {
 });
 
 export default axios;
-
-
--- web/api/axios.jsx
+++ web/api/axios.jsx
@@ -1,30 +1,8 @@
-import axiosClient from "axios";
-import { getToken } from "/services/auth-storage";
-
-const axios = axiosClient.create({
-  baseURL: "http://10.90.129.170:8000/api",
-  headers: {
-    Accept: "Content-Type: application/json",
-  },
-});
-
-axios.interceptors.request.use(async (req) => {
-  const token = await getToken();
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-  return req;
-});
-
-export default axios;
-
-
-
 // import axiosClient from "axios";
 // import { getToken } from "/services/auth-storage";
-// const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
+
 // const axios = axiosClient.create({
-//   baseURL: import.meta.env.VITE_API_URL,
+//   baseURL: "http://10.90.129.170:8000/api",
 //   headers: {
 //     Accept: "Content-Type: application/json",
 //   },
@@ -39,3 +17,25 @@ export default axios;
 // });
 
 // export default axios;
+
+
+
+import axiosClient from "axios";
+import { getToken } from "/services/auth-storage";
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
+const axios = axiosClient.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    Accept: "Content-Type: application/json",
+  },
+});
+
+axios.interceptors.request.use(async (req) => {
+  const token = await getToken();
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
+});
+
+export default axios;
